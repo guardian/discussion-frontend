a57231092094d8e70d43de3dd47adab0d55cc8ed
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-09-20 14:24:15 +0100

    Intersection observer shows and hide the banner

--- src/ui/sticky-banner/banner.js
+++ src/ui/sticky-banner/banner.js
@@ -3,18 +3,46 @@ import CloseButton from '../button/close';
 import FauxLink from '../faux-link/faux-link';
 import bubbleCentral from '../../images/bubble-central.svg';
 import { thousands } from '../../utils/format';
+import { observeRatio } from '../../utils/intersection-observer';
 
 class StickyBanner extends React.Component {
     constructor () {
         super();
+        this.state = {
+            // The banner should only be visible when the user is viewing the body content.
+            // Dismiss it when it reaches the bottom of the article (not of the page).
+            inScrollTarget: false
+        };
+    }
+
+    componentDidMount () {
+        const options = {
+            rootMargin: '-20% 0px -25%',
+            threshold: 0
+        };
+        this.observer = observeRatio(
+            document.getElementsByClassName('js-article__body')[0],
+            options,
+            visible => {
+                this.setState({ inScrollTarget: visible });
+            }
+        );
+    }
+
+    componentWillUnmount () {
+        if (this.observer) {
+            this.observer.disconnect();
+        }
     }
 
     render ({
         commentsCount,
         dismissable
     } = this.props) {
+        const containerClasses = [styles.bannerContainer, this.state.inScrollTarget ? styles.visible : styles.hidden].join(' ');
+
         return (
-            <div className={styles.bannerContainer} data-link-name="comments sticky banner">
+            <div className={containerClasses} data-link-name="comments sticky banner">
                 <FauxLink href="#comments">
                     <div className={styles.bannerMessage}>
                         <div className={styles.options}>
