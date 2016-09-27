e0a9eab40626455a46a0623a51d9d18b8b8e5a4c
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-09-27 09:43:51 +0100

    Include fetch polyfill and inject Promise

--- src/utils/net.js
+++ src/utils/net.js
@@ -1,21 +1,27 @@
-export function json (path) {
-    return fetch(path, {
-        mode: 'cors'
-    }).then(resp => {
-        return resp.ok ? resp.json() : Promise.reject(new Error('fetch error: ' + resp.statusText));
-    });
-}
+import 'whatwg-fetch';
 
-export function jsonp (path) {
-    return new Promise(function(resolve, reject) {
-        const jsonpCallback = 'jp_' + Math.floor((Math.random() * 100)) + new Date().getTime();
-        const jsonpPath = [path, 'callback=' + jsonpCallback].join(path.indexOf('?') === -1 ? '?' : '&');
-        window[jsonpCallback] = resolve;
-        const script = document.createElement('script');
-        script.src = jsonpPath;
-        script.onerror = function() {
-            reject(new Error('JSONP network error on ' + jsonpPath));
-        };
-        document.head.appendChild(script);
-    });
+export function inject (Promise) {
+    function json (path) {
+        return fetch(path, {
+            mode: 'cors'
+        }).then(resp => {
+            return resp.ok ? resp.json() : Promise.reject(new Error('fetch error: ' + resp.statusText));
+        });
+    }
+
+    function jsonp (path) {
+        return new Promise(function(resolve, reject) {
+            const jsonpCallback = 'jp_' + Math.floor((Math.random() * 100)) + new Date().getTime();
+            const jsonpPath = [path, 'callback=' + jsonpCallback].join(path.indexOf('?') === -1 ? '?' : '&');
+            window[jsonpCallback] = resolve;
+            const script = document.createElement('script');
+            script.src = jsonpPath;
+            script.onerror = function() {
+                reject(new Error('JSONP network error on ' + jsonpPath));
+            };
+            document.head.appendChild(script);
+        });
+    }
+
+    return { json, jsonp };
 }
