dd269f4a47ee03978894e5c19cfbff46dae0654d
Calum Campbell <campbell.cj@hotmail.co.uk>
2017-06-29 14:55:12 +0100

    deploy.json to rif-raff.yaml

--- bin/riffraff.js
+++ bin/riffraff.js
@@ -2,31 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const listFiles = require('./lib/list');
-const deploy = require('../deploy.json');
+const yaml = require('js-yaml');
 process.env.ARTEFACT_PATH = path.join(__dirname, '../');
 const riffraff = require('node-riffraff-artefact');
 
 const SOURCE_PATH = path.join(__dirname, '../dist/hashed');
 const DESTINATION_PATH = path.join(__dirname, '../tmp/riffraff');
+const deploy = yaml.safeLoad(fs.readFileSync('riff-raff.yaml', 'utf8'));
 
 mkdirp.sync(DESTINATION_PATH);
-const pack = getPackage(deploy);
 
-if (!pack) {
-    process.exit(2);
-}
-copyHashedFiles(DESTINATION_PATH + '/packages/' + pack);
-copyDeployJson(DESTINATION_PATH, deploy);
-uploadToRiffRaff().then(function() {
-    // eslint-disable-next-line no-console
-    console.log('Upload done');
-}).catch(function(error) {
-    // eslint-disable-next-line no-console
-    console.error('Upload error', error);
-});
-
-
-function copyHashedFiles (destinationPath) {
+const copyHashedFiles = (destinationPath) => {
     mkdirp.sync(destinationPath);
 
     listFiles(SOURCE_PATH)
@@ -35,19 +21,35 @@ function copyHashedFiles (destinationPath) {
         .pipe(fs.createWriteStream(path.join(destinationPath, path.basename(file))));
     });
 
-}
+};
 
-function getPackage (json) {
-    return Object.keys(json.packages)
-    .filter(key => json.packages[key].type === 'aws-s3')[0];
-}
+const getPackage = (yaml) => {
 
-function copyDeployJson (destinationPath, json) {
-    fs.writeFileSync(path.join(destinationPath, 'deploy.json'), JSON.stringify(json));
-}
+    return Object.keys(yaml).filter(key => yaml[key].type === 'aws-s3')[0];
+};
 
-function uploadToRiffRaff () {
+const copyRiffRaffYaml = (destinationPath, yaml) => {
+    fs.writeFileSync(path.join(destinationPath, 'riff-raff.yaml'), JSON.stringify(yaml));
+};
+
+const uploadToRiffRaff = () => {
     riffraff.settings.leadDir = path.join(__dirname, '../tmp/riffraff');
 
     return riffraff.s3FilesUpload();
+};
+
+const pack = getPackage(deploy);
+
+if (!pack) {
+    process.exit(2);
 }
+
+copyHashedFiles(DESTINATION_PATH + '/packages/' + pack);
+copyRiffRaffYaml(DESTINATION_PATH, deploy);
+uploadToRiffRaff().then(function() {
+    // eslint-disable-next-line no-console
+    console.log('Upload done');
+}).catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error('Upload error', error);
+});
